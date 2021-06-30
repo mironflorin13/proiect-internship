c0e53c65a0dd1635f13826653bd2d6d6966a1f42
Miron Florin <mironflorin13@gmail.com>
2021-06-30 17:06:54 +0300

    added in progress complete chalenge inside app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import LeftHandPanel from "./components/LeftHandPanel";
-
+import InProgressCompleteChallenge from "./components/inProgressCompleteChallange/InProgressCompleteChallenge";
 import "./app.scss";
 import users from "./mockFunctions/users.js";
 
@@ -9,6 +9,7 @@ function App() {
   return (
     <div>
       <LeftHandPanel userAvatar={users} />
+      <InProgressCompleteChallenge />
     </div>
   );
 }
