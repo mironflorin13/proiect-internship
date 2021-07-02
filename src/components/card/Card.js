742ccb1baa98b24a76c19c48ec6dd1a0b9177dd7
Miron Florin <mironflorin13@gmail.com>
2021-07-02 16:26:37 +0300

    modify Card structure and card scss

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -3,46 +3,28 @@ import "./card.scss";
 import Button from "../button/Button";
 
 const Card = ({ data }) => {
-  const showStatus = () => {
-    return (
-      <div
-        className={`status ${data.status === "validated" ? "green" : "red"}`}
-      >
-        {data.status}
-      </div>
-    );
-  };
+  const completed =
+    data.status === "validated" || data.status === "denied" ? data.status : "";
 
   return (
-    <div
-      className={`card ${
-        data.status !== "available"
-          ? data.status === "validated"
-            ? "border-top-green"
-            : "border-top-red"
-          : ""
-      }`}
-    >
+    <div className={`card ${completed}`}>
       <h2 className="title">{data.title}</h2>
+
       <div className="xp-and-credits">
         <h3 className="xp">{data.xp} XP</h3>
         <h3 className="credits">{data.credits} CREDITS</h3>
       </div>
+
       <p className="description">{data.description}</p>
+
       <div className="bottom">
-        {data.status !== "available" ? (
-          <div
-            className={`status ${
-              data.status === "validated" ? "green" : "red"
-            }`}
-          >
-            {data.status}
-          </div>
-        ) : (
+        {data.status === "available" ? (
           <div className="buttons-container">
             <Button type="btn secondary" value="Quit" />
             <Button type="btn primary flex-width-max" value="Completed" />
           </div>
+        ) : (
+          <h3>{data.status}</h3>
         )}
       </div>
     </div>
