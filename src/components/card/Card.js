f2fd8528897a3ae8088c5c646868ddf32cfd2b93
Miron Florin <mironflorin13@gmail.com>
2021-07-05 11:11:04 +0300

    modify card structure/ add button class

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -2,29 +2,37 @@ import React from "react";
 import "./card.scss";
 import Button from "../button/Button";
 
-const Card = ({ data }) => {
-  const completed =
-    data.status === "validated" || data.status === "denied" ? data.status : "";
+const Card = ({
+  status,
+  title,
+  xp,
+  credits,
+  description,
+  button1Message,
+  button2Message,
+}) => {
+  const completed = status === "validated" || status === "denied" ? status : "";
+  const hide = button1Message === "" ? "hide-btn" : "";
 
   return (
     <div className={`card ${completed}`}>
-      <h2 className="title">{data.title}</h2>
+      <h2 className="title">{title}</h2>
 
       <div className="xp-and-credits">
-        <h3 className="xp">{data.xp} XP</h3>
-        <h3 className="credits">{data.credits} CREDITS</h3>
+        <h3 className="xp">{xp} XP</h3>
+        <h3 className="credits">{credits} CREDITS</h3>
       </div>
 
-      <p className="description">{data.description}</p>
+      <p className="description">{description}</p>
 
       <div className="bottom">
-        {data.status === "available" ? (
+        {completed === "" ? (
           <div className="buttons-container">
-            <Button type="btn secondary" value="Quit" />
-            <Button type="btn primary flex-width-max" value="Completed" />
+            <Button type={`btn secondary ${hide}`} value={button1Message} />
+            <Button type="btn primary flex-width-max" value={button2Message} />
           </div>
         ) : (
-          <h3>{data.status}</h3>
+          <h3>{status}</h3>
         )}
       </div>
     </div>
