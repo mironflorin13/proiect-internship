5989a25dbde62a3e4c3539fc26ac834b9c5c62f0
Miron Florin <mironflorin13@gmail.com>
2021-07-12 09:55:55 +0300

    renamed the files

--- src/application.js
+++ src/application.js
@@ -3,10 +3,10 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import getUserInfo from "./mockFunctions/get-user-info.js";
 import LeftHandPanel from "./components/left-hand-panel/left-hand-panel";
-import Overview from "./all-pages/overview";
-import Challenges from "./all-pages/challenges";
-import Demo from "./all-pages/demo";
-import Shop from "./all-pages/shop";
+import Overview from "./pages/overview";
+import Challenges from "./pages/challenges";
+import Demo from "./pages/demo";
+import Shop from "./pages/shop";
 import "./application.scss";
 import users from "./mockFunctions/users.js";
 
