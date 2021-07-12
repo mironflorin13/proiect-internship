4e507ed69d633335f0952c95cf9689c24888804f
murarasu <murarasu@arezzosky.com>
2021-07-12 10:53:45 +0300

    update

--- src/application.js
+++ src/application.js
@@ -2,20 +2,12 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import getUserInfo from "./mockFunctions/get-user-info.js";
-<<<<<<< Updated upstream:src/application.js
 import LeftHandPanel from "./components/left-hand-panel/left-hand-panel";
-=======
-import LeftHandPanel from "./components/leftHandPanel/left-hand-panel";
->>>>>>> Stashed changes:src/App.js
 import Overview from "./pages/overview";
 import Challenges from "./pages/challenges";
 import Demo from "./pages/demo";
 import Shop from "./pages/shop";
-<<<<<<< Updated upstream:src/application.js
 import "./application.scss";
-=======
-import "./app.scss";
->>>>>>> Stashed changes:src/App.js
 import users from "./mockFunctions/users.js";
 
 function App() {
--- src/pages/demo.js
+++ src/pages/demo.js
@@ -1,24 +1,14 @@
 import React, { useState } from "react";
 
-<<<<<<< Updated upstream:src/pages/demo.js
 import "../components/left-hand-panel/left-hand-panel.scss";
 import "./demo.scss";
 import ExperienceBar from "../components/left-hand-panel/experience-bar";
-=======
-import "../components/leftHandPanel/LeftHandPanel.scss";
-import "./Demo.scss";
-import ExperienceBar from "../components/leftHandPanel/experience-bar";
->>>>>>> Stashed changes:src/pages/Demo.js
 
 function Demo() {
   const [count, setCount] = useState(10);
 
   function incrementCountHandler() {
-<<<<<<< Updated upstream:src/pages/demo.js
-    setCount(prevCount => prevCount + 1);
-=======
     setCount(prevCount => prevCount + 5);
->>>>>>> Stashed changes:src/pages/Demo.js
   }
 
   return (
