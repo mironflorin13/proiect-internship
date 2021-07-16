a7fe20c62f00c055115283fea285fe9513966032
Miron Florin <mironflorin13@gmail.com>
2021-07-16 15:50:03 +0300

    use children prop and variables

--- src/components/shop-card/shop-card.js
+++ src/components/shop-card/shop-card.js
@@ -1,17 +1,14 @@
 import React from "react";
 
-import Button from "../button/button";
 import "./shop-card.scss";
 
-const ShopCard = ({ title, description }) => (
+const ShopCard = ({ title, description, children }) => (
   <div className="shop-card">
     <div className="image"></div>
     <div className="bottom-section">
       <div className="title">{title}</div>
       <div className="description">{description}</div>
-      <div className="buttons-container">
-        <Button type="btn primary flex-width-max" value="Buy-50 Credits" />
-      </div>
+      <div className="buttons-container">{children}</div>
     </div>
   </div>
 );
